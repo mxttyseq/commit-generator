c93773c609fce8d2be2b722102056bee8470dbe5
Matěj Bureš <matejbures11011@outlook.com>
2026-01-31 16:17:38 +0100

    feat(frontend): Added main page.

---
+++ frontend/js/config.js
@@ -0,0 +1,1 @@
+export const API_URL = "http://localhost:123/api/v1/ai";
---
+++ frontend/js/script.js
@@ -0,0 +1,25 @@
+import { API_URL } from "./config.js";
+
+const button = document.getElementById("btn");
+
+
+button.addEventListener("click", () => {
+    const input = document.getElementById("diff");
+    const val = input.value;
+    const data = {data: val};
+
+    fetch(API_URL, {
+       method: "POST",
+       headers: {
+           "Content-Type": "application/json"
+       },
+       body: JSON.stringify(data)
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log("Api response:", data)
+    })
+    .catch(error => {
+        console.log("Error", error)
+    })
+});
